28cb7937b8657a3d5209880e6501cda558247565
joshMorgan06 <joshua.morgan3@ontariotechu.net>
2023-02-26 20:22:12 -0500

    boogaloo

--- FrogPicnicDELUXE/Assets/Josh's Folder/Scripts/CameraLUT.cs
+++ FrogPicnicDELUXE/Assets/Josh's Folder/Scripts/CameraLUT.cs
@@ -7,26 +7,16 @@ public class CameraLUT : MonoBehaviour
     //public Shader myShader = null;
     public Material m_renderMaterial;
     public Material normalLUT;
-    public Material warmLUT;
-    public Material coldLUT;
-    public Material customLUT;
+    public Material nightLUT;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha5))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             m_renderMaterial = normalLUT;
-        }else if (Input.GetKeyDown(KeyCode.Alpha6))
+        }else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            m_renderMaterial = warmLUT;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha7))
-        {
-            m_renderMaterial = coldLUT;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha8))
-        {
-            m_renderMaterial = customLUT;
+            m_renderMaterial = nightLUT;
         }
     }
 
